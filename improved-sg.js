fb5e00e8444980044c5adb582c2f44873c8bf021
guilima <gui.lima30@gmail.com>
2018-03-22 02:56:36 -0300

    refactor(improved-sg): Prettify and adjust a set of bugfix

--- improved-sg.js
+++ improved-sg.js
@@ -1,178 +1,200 @@
 // ==UserScript==
-// @name         Improved Steamgifts
-// @namespace    improvedSG
-// @version      1.0.1
-// @run-at       document-start
-// @include		 *steamgifts.com*
-// @description  Improves steamgifts.com user experience
-// @author       Guilherme Lima
-// @require      https://raw.githubusercontent.com/guilima/Improved-Steamgifts/master/improved-sg.style.js
-// @downloadURL  https://raw.githubusercontent.com/guilima/Improved-Steamgifts/master/improved-sg.js
-// @updateURL    https://raw.githubusercontent.com/guilima/Improved-Steamgifts/master/improved-sg.js
-// @homepage     https://github.com/guilima/Improved-Steamgifts
-// @grant        none
-// @license      MIT
+// @name          Improved Steamgifts
+// @namespace     improvedSG
+// @version       1.0.1
+// @run-at        document-start
+// @include       *steamgifts.com*
+// @description   Improves steamgifts.com user experience
+// @author        Guilherme Lima
+// @require       https://raw.githubusercontent.com/guilima/Improved-Steamgifts/master/improved-sg.style.js
+// @downloadURL   https://raw.githubusercontent.com/guilima/Improved-Steamgifts/master/improved-sg.js
+// @updateURL     https://raw.githubusercontent.com/guilima/Improved-Steamgifts/master/improved-sg.js
+// @homepage      https://github.com/guilima/Improved-Steamgifts
+// @grant         none
+// @license       MIT
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    let currentPage = 1,
-        lastGiveawayRow;
-    const btnEntryGiveaway = `<button class="entry-giveaway"><i class="fa fa-plus-circle" style="vertical-align: text-bottom;"></i> Enter Giveaway</button>&nbsp;`,
-          btnLoadMore = `<button class="load-more-giveaway"><i></i> Load more</button>`,
-          queryParamType = new URL(window.location.href).searchParams.get("type");
+  'use strict';
+  let currentPage = 1,
+    lastGiveawayRow,
+    loadMoreBtn;
+  const entryGiveawayBtnString = `<button class="entry-giveaway"><i class="fa fa-plus-circle" style="vertical-align: text-bottom;"></i> Enter Giveaway</button>&nbsp;`,
+    loadMoreBtnString = `<button class="load-more-giveaway"><i></i> Load more</button>`,
+    queryParamType = new URL(window.location.href).searchParams.get('type');
 
-
-    document.addEventListener("DOMContentLoaded", function() {
-        lastGiveawayRow = document.querySelector('div[style*="padding-top: 35px; padding-bottom: 10px; display: flex; justify-content: center;"]');
-        document.querySelectorAll(".giveaway__row-inner-wrap.is-faded").forEach(e => e.parentNode.remove());
-        const giveaways = [...document.querySelectorAll('.giveaway__row-outer-wrap')];
-        lastGiveawayRow.insertAdjacentHTML('afterbegin', btnLoadMore);
-        giveaways.forEach(giveaway => {
-            const giveawayGameHeading = giveaway.querySelector('.giveaway__heading');
-            giveawayGameHeading.insertAdjacentHTML('afterbegin', btnEntryGiveaway);
-        });
-        document.querySelector('.load-more-giveaway').addEventListener('click', loadMore);
-        document.querySelectorAll('.entry-giveaway').forEach(btnGiveaway => btnGiveaway.addEventListener('click', entryGiveaway));
-        document.querySelectorAll('.giveaway__hide').forEach(btnHideGiveaway => btnHideGiveaway.addEventListener('click', hideGiveaway));
+  document.addEventListener('DOMContentLoaded', function() {
+    lastGiveawayRow = document.querySelector(
+      'div[style*="padding-top: 35px; padding-bottom: 10px; display: flex; justify-content: center;"]'
+    );
+    document.querySelectorAll('.giveaway__row-inner-wrap.is-faded').forEach(e => e.parentNode.remove());
+    const giveaways = [...document.querySelectorAll('.giveaway__row-outer-wrap')];
+    lastGiveawayRow.insertAdjacentHTML('afterbegin', loadMoreBtnString);
+    loadMoreBtn = document.querySelector('.load-more-giveaway');
+    giveaways.forEach(giveaway => {
+      const giveawayGameHeading = giveaway.querySelector('.giveaway__heading');
+      giveaway.querySelector('.giveaway__hide').removeAttribute('data-popup');
+      giveawayGameHeading.insertAdjacentHTML('afterbegin', entryGiveawayBtnString);
     });
+    loadMoreBtn.addEventListener('click', loadMore);
+    document
+      .querySelectorAll('.entry-giveaway')
+      .forEach(btnGiveaway => btnGiveaway.addEventListener('click', entryGiveaway));
+    document
+      .querySelectorAll('.giveaway__hide')
+      .forEach(btnHideGiveaway => btnHideGiveaway.addEventListener('click', hideGiveaway));
+  });
 
-    function entryGiveaway() {
-        /* jshint validthis: true */
-        const token = document.querySelector("input[name='xsrf_token']").value,
-              isInsert = !this.classList.contains('delete'),
-              myPointsElem = document.querySelector('.nav__points'),
-              myPoints = parseFloat(myPointsElem.innerText.match(/[0-9*]/g).join('')),
-              gamePoints = parseFloat(this.parentNode.querySelector('.giveaway__heading__thin:last-of-type').innerText.match(/[0-9*]/g).join('')),
-              gameName = this.parentNode.querySelector('.giveaway__heading__name').innerText,
-              gameCode = this.parentNode.querySelector('.giveaway__heading__name').getAttribute('href').split('/')[2],
-              action = isInsert ? 'entry_insert' : 'entry_delete',
-              bgColor = isInsert ? '#c9b76a' : '#9ac96a',
-              textColor = isInsert ? '#505302' : '#305302',
-              formData = new FormData();
-        formData.append('xsrf_token', token);
-        formData.append('code', gameCode);
-        formData.append('do', action);
-        fetch('https://www.steamgifts.com/ajax.php', {
-            method: 'POST',
-            credentials: 'same-origin',
-            body: new URLSearchParams(formData),
-            headers: new Headers({
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-            })
-        }).then(res => {
-            console.info(`${gameName} - ${action}`);
-            this.style.backgroundColor = bgColor;
-            this.style.color = textColor;
-            this.querySelector('.fa').classList.toggle('fa-minus-circle', isInsert);
-            myPointsElem.innerText = isInsert ? myPoints - gamePoints : myPoints + gamePoints;
-        }).catch(error => console.error('Error:', error));
+  function entryGiveaway() {
+    /* jshint validthis: true */
+    const token = document.querySelector("input[name='xsrf_token']").value,
+      isInsert = !this.classList.contains('delete'),
+      iconClass = this.firstChild.classList,
+      myPointsElem = document.querySelector('.nav__points'),
+      myPoints = parseFloat(myPointsElem.innerText.match(/[0-9*]/g).join('')),
+      gamePoints = parseFloat(
+        this.parentNode
+          .querySelector('.giveaway__heading__thin:last-of-type')
+          .innerText.match(/[0-9*]/g)
+          .join('')
+      ),
+      gameName = this.parentNode.querySelector('.giveaway__heading__name').innerText,
+      gameCode = this.parentNode
+        .querySelector('.giveaway__heading__name')
+        .getAttribute('href')
+        .split('/')[2],
+      action = isInsert ? 'entry_insert' : 'entry_delete',
+      bgColor = isInsert ? '#c9b76a' : '#9ac96a',
+      textColor = isInsert ? '#505302' : '#305302',
+      formData = new FormData();
+    formData.append('xsrf_token', token);
+    formData.append('code', gameCode);
+    formData.append('do', action);
+    this.firstChild.className = '';
+    this.setAttribute('disabled', '');
+    iconClass.add('fa', 'fa-refresh', 'fa-spin');
+    fetch('https://www.steamgifts.com/ajax.php', {
+      method: 'POST',
+      credentials: 'same-origin',
+      body: new URLSearchParams(formData),
+      headers: new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      })
+    })
+      .then(() => {
+        console.info(`${gameName} - ${action}`);
+        this.style.backgroundColor = bgColor;
+        this.style.color = textColor;
+        iconClass.add('fa');
+        iconClass.toggle('fa-minus-circle', isInsert);
         this.classList.toggle('delete');
-    }
+        myPointsElem.innerText = isInsert ? myPoints - gamePoints : myPoints + gamePoints;
+      })
+      .catch(error => console.error('Error:', error))
+      .then(() => {
+        iconClass.add('fa', 'fa-plus-circle');
+        iconClass.remove('fa-spin');
+        this.removeAttribute('disabled');
+      });
+  }
 
-    function hideGiveaway(){
-        /* jshint validthis: true */
-        const game = this.closest(".giveaway__row-outer-wrap"),
-              gameName = game.querySelector('.giveaway__heading__name').innerText,
-              modalBody = document.querySelector(".popup--hide-games"),
-              formData = new FormData();
-        formData.append('xsrf_token', document.querySelector("input[name='xsrf_token']").value);
-        formData.append('game_id', game.getAttribute("data-game-id"));
-        formData.append('do', 'hide_giveaways_by_game_id');
-        fetch('https://www.steamgifts.com/ajax.php', {
-            method: 'POST',
-            credentials: 'same-origin',
-            body: new URLSearchParams(formData),
-            headers: new Headers({
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-            })
-        }).then(res => {
-            console.info(`${gameName} - Removed`);
-            game.parentNode.removeChild(game);
-        }).catch(error => console.error('Error:', error));
-        $(modalBody).bPopup().close();
-    }
+  function hideGiveaway() {
+    /* jshint validthis: true */
+    const game = this.closest('.giveaway__row-outer-wrap'),
+      gameName = game.querySelector('.giveaway__heading__name').innerText,
+      formData = new FormData();
+    formData.append('xsrf_token', document.querySelector("input[name='xsrf_token']").value);
+    formData.append('game_id', game.getAttribute('data-game-id'));
+    formData.append('do', 'hide_giveaways_by_game_id');
+    fetch('https://www.steamgifts.com/ajax.php', {
+      method: 'POST',
+      credentials: 'same-origin',
+      body: new URLSearchParams(formData),
+      headers: new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      })
+    })
+      .then(() => {
+        console.info(`${gameName} - Removed`);
+        game.parentNode.removeChild(game);
+      })
+      .catch(error => console.error('Error:', error));
+  }
 
-    function loadMore() {
-        /* jshint validthis: true */
-        const $this = this || document.querySelector('.load-more-giveaway'),
-              loadingClass = ['fa', 'fa-refresh', 'fa-spin', 'fa-2x', 'fa-fw'],
-              type = queryParamType ? `&type=${queryParamType}` : '';
+  function loadMore() {
+    const loadingClass = ['fa', 'fa-refresh', 'fa-spin', 'fa-2x', 'fa-fw'],
+      type = queryParamType ? `&type=${queryParamType}` : '';
+    loadMoreBtn.setAttribute('disabled', '');
+    loadMoreBtn.firstChild.classList.add(...loadingClass);
+    fetch(`https://www.steamgifts.com/giveaways/search?page=${currentPage}${type}`, {
+      method: 'GET',
+      credentials: 'same-origin'
+    })
+      .then(res => {
+        return res.text();
+      })
+      .then(function(data) {
         currentPage++;
-        $this.setAttribute('disabled', '');
-        $this.firstChild.classList.add(...loadingClass);
-        fetch(`https://www.steamgifts.com/giveaways/search?page=${currentPage}${type}`, {
-            method: 'GET',
-            credentials: 'same-origin'
-        }).then(res => {
-            return res.text();
-        }).then(function(data) {
-            console.info(`Page ${currentPage} - loaded`);
-            const regexHref = /<a class="giveaway__heading__name" href="\/giveaway\/(\w*)\/([A-Za-z0-9-]*)">([a-zA-Z0-9-: ]*)/g,
-                  result = [];
-            let execArray,
-                crawlerData = data.replace(/\s+/g, " "); //remove big whitespaces
-            crawlerData = crawlerData.substring(crawlerData.indexOf('<div class="page__heading">')); //remove before
-            crawlerData = crawlerData.substring(0, crawlerData.indexOf('<div class="pagination">')); //remove after
-            crawlerData = crawlerData.replace(/<div> <div class="giveaway__row-outer-wrap"/, `<div class="giveaway-page-${currentPage}"> <div class="giveaway__row-outer-wrap`); // add id currentPage
-            crawlerData = crawlerData.replace(/<h2 class="giveaway__heading"> <a class="giveaway__heading__name"/g, `<h2 class="giveaway__heading">${btnEntryGiveaway}<a class="giveaway__heading__name"`); // add button
-            lastGiveawayRow.insertAdjacentHTML('beforebegin', crawlerData);
+        console.info(`Page ${currentPage} - loaded`);
+        let crawlerData = data.replace(/\s+/g, ' '); //remove big whitespaces
+        crawlerData = crawlerData.substring(crawlerData.indexOf('<div class="page__heading">')); //remove before
+        crawlerData = crawlerData.substring(0, crawlerData.indexOf('<div class="pagination">')); //remove after
+        crawlerData = crawlerData.replace(
+          /<div> <div class="giveaway__row-outer-wrap"/,
+          `<div class="giveaway-page-${currentPage}"> <div class="giveaway__row-outer-wrap`
+        ); // add id currentPage
+        crawlerData = crawlerData.replace(
+          /<h2 class="giveaway__heading"> <a class="giveaway__heading__name"/g,
+          `<h2 class="giveaway__heading">${entryGiveawayBtnString}<a class="giveaway__heading__name"`
+        ); // add button
+        crawlerData = crawlerData.replace(/data-popup="popup--hide-games"/g, ''); //remove popup
+        lastGiveawayRow.insertAdjacentHTML('beforebegin', crawlerData);
 
-            //remove load button
-            if(!crawlerData || document.querySelectorAll(`.giveaway-page-${currentPage} .giveaway__row-inner-wrap`).length <= 49) {
-                lastGiveawayRow.insertAdjacentHTML('beforebegin', '<p style="text-align: center;">No more games. :(</p>');
-                $this.remove();
-            }
+        loadMoreBtn.firstChild.classList.remove(...loadingClass);
+        loadMoreBtn.removeAttribute('disabled');
 
-            //generate game params for automatic service calls
-            while ((execArray = regexHref.exec(crawlerData)) !== null) {
-                result.push({code:execArray[1],slug:execArray[2],name:execArray[3]});
-            }
-            //console.log(result);
-
-            $this.firstChild.classList.remove(...loadingClass);
-            $this.removeAttribute('disabled');
-            document.querySelectorAll(`.giveaway-page-${currentPage} .giveaway__row-inner-wrap.is-faded`).forEach(e => e.parentNode.remove());
-            document.querySelectorAll(`.giveaway-page-${currentPage} .entry-giveaway`).forEach(btnGiveaway => btnGiveaway.addEventListener('click', entryGiveaway));
-            document.querySelectorAll(`.giveaway-page-${currentPage} .giveaway__hide`).forEach(btnHideGiveaway => btnHideGiveaway.addEventListener('click', hideGiveaway));
-        });
-    }
-
-    function isScrolledIntoView(elem, increaseDistance) {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
-
-        var elemTop = $(elem).offset().top ;
-        var elemBottom = elemTop + $(elem).height() - increaseDistance;
-
-        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-    }
-
-    function autoLoad(scroll_pos) {
-        if(isScrolledIntoView('.load-more-giveaway', 1400) &&
-           document.querySelector('.load-more-giveaway') &&
-           !document.querySelector('.load-more-giveaway').firstChild.classList.contains('fa-spin')) {
-            loadMore();
+        //remove load button
+        if (
+          !crawlerData ||
+          document.querySelectorAll(`.giveaway-page-${currentPage} .giveaway__row-inner-wrap`).length <= 49
+        ) {
+          lastGiveawayRow.insertAdjacentHTML(
+            'beforebegin',
+            '<p style="text-align: center;">No more games. :(</p>'
+          );
+          loadMoreBtn.remove();
+          loadMoreBtn = null;
         }
+
+        document
+          .querySelectorAll(`.giveaway-page-${currentPage} .giveaway__row-inner-wrap.is-faded`)
+          .forEach(e => e.parentNode.remove());
+        document
+          .querySelectorAll(`.giveaway-page-${currentPage} .entry-giveaway`)
+          .forEach(btnGiveaway => btnGiveaway.addEventListener('click', entryGiveaway));
+        document
+          .querySelectorAll(`.giveaway-page-${currentPage} .giveaway__hide`)
+          .forEach(btnHideGiveaway => btnHideGiveaway.addEventListener('click', hideGiveaway));
+      });
+  }
+
+  let ticking = false;
+
+  window.addEventListener('scroll', function() {
+    if (
+      !loadMoreBtn ||
+      loadMoreBtn.getBoundingClientRect().top >= 2000 ||
+      loadMoreBtn.hasAttribute('disabled')
+    ) {
+      return;
     }
 
-    let last_known_scroll_position = 0,
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        loadMore();
         ticking = false;
-
-    window.addEventListener('scroll', function(e) {
-        if(!document.querySelector('.load-more-giveaway')) return;
-        last_known_scroll_position = window.scrollY;
-
-        if (!ticking) {
-
-            window.requestAnimationFrame(function() {
-                autoLoad(last_known_scroll_position);
-                ticking = false;
-            });
-
-            ticking = true;
-
-        }
-
-    });
+      });
+      ticking = true;
+    }
+  });
 })();
--- improved-sg.style.js
+++ improved-sg.style.js
@@ -1,50 +1,50 @@
 // ==UserScript==
-// @name         Improved Steamgifts - Styles
-// @run-at       document-start
-// @namespace    improvedSteamgifts_styles.js
-// @version      0.1
-// @description  Improves steamgifts.com user experience
-// @author       Guilherme Lima
-// @match        *steamgifts.com*
-// @grant        none
-// @license      MIT
+// @name          Improved Steamgifts - Styles
+// @run-at        document-start
+// @namespace     improvedSteamgifts_styles.js
+// @version       0.1
+// @description   Improves steamgifts.com user experience
+// @author        Guilherme Lima
+// @match         *steamgifts.com*
+// @grant         none
+// @license       MIT
 // ==/UserScript==
 
 (function() {
-	'use strict';
-	const style = document.createElement('style'),
-				css = `
-					.is-faded { display: none!important }
+  'use strict';
+  const style = document.createElement('style'),
+    css = `
+      body > header {
+        position: fixed;
+        width: 100%;
+        z-index: 100;
+        top: 0px;
+      }
 
-					body > header {
-						position: fixed;
-						width: 100%;
-						z-index: 100;
-						top: 0px;
-					}
+      .featured__container { margin-top: 38px; }
 
-					.featured__container { margin-top: 38px; }
+      .b-modal,
+      .is-faded,
+      .popup--hide-games { display: none!important;}
 
-					.b-modal { background-color: transparent!important; }
+      .giveaway__heading { margin-bottom: 5px; }
 
-					.giveaway__heading { margin-bottom: 5px; }
+      .entry-giveaway {
+        cursor: pointer;
+        padding: 4px 6px;
+        font-weight:bold;
+        background-color: #9ac96a;
+        color: #305302;
+        border-radius: 2px;
+      }
 
-					.entry-giveaway {
-						cursor: pointer;
-						padding: 4px 6px;
-						font-weight:bold;
-						background-color: #9ac96a;
-						color: #305302;
-						border-radius: 2px;
-					}
-
-					.load-more-giveaway {
-						cursor: pointer;
-						padding: 12px;
-						font-size: 16px;
-						background: #03a7b4;
-						color: #fff;
-					}`;
-	style.appendChild(document.createTextNode(css));
-	document.head.appendChild(style);
+      .load-more-giveaway {
+        cursor: pointer;
+        padding: 12px;
+        font-size: 16px;
+        background: #03a7b4;
+        color: #fff;
+      }`;
+  style.appendChild(document.createTextNode(css));
+  document.head.appendChild(style);
 })();
